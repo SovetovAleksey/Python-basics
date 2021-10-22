d3742e852180b93bc0e359daf9d18b301c6c1a6f
SovetovAleksey <91490501+SovetovAleksey@users.noreply.github.com>
2021-10-22 18:21:51 +0300

    Add files via upload

--- 1 quest.py
+++ 1 quest.py
@@ -1,31 +1,40 @@
-# 1. Реализовать класс Matrix (матрица). Обеспечить перегрузку конструктора класса (метод __init__()),
-# который должен принимать данные (список списков) для формирования матрицы.
-# Подсказка: матрица — система некоторых математических величин, расположенных в виде прямоугольной схемы.
-# Примеры матриц вы найдете в методичке.
-# Следующий шаг — реализовать перегрузку метода __str__() для вывода матрицы в привычном виде.
-# Далее реализовать перегрузку метода __add__() для реализации операции сложения
-# двух объектов класса Matrix (двух матриц). Результатом сложения должна быть новая матрица.
-# Подсказка: сложение элементов матриц выполнять поэлементно — первый элемент первой строки первой матрицы
-# складываем с первым элементом первой строки второй матрицы и т.д.
+# Реализовать класс «Дата», функция-конструктор которого должна принимать дату в виде строки формата «день-месяц-год».
+# В рамках класса реализовать два метода.
+# Первый, с декоратором @classmethod, должен извлекать число, месяц, год и преобразовывать их тип к типу «Число».
+# Второй, с декоратором @staticmethod, должен проводить валидацию числа, месяца и года (например, месяц — от 1 до 12).
+# Проверить работу полученной структуры на реальных данных.
 
-class Matrix():
+class Data:
+    def __init__(self, day, month, year):
+        self.day = str(day)
+        self.month = str(month)
+        self.year = str(year)
 
-    def __init__(self, list_1, list_2):
-        self.list_1 = list_1
-        self.list_2 = list_2
+    @classmethod
+    def extract(cls, day, month, year):
+        my_data = []
 
-    def __add__(self):
-        matr = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+        my_data.append(day)
+        my_data.append(month)
+        my_data.append(year)
 
-        for i in range(len(self.list_1)):
-            for j in range(len(self.list_2[i])):
-                matr[i][j] = self.list_1[i][j] + self.list_2[i][j]
-        return str('\n'.join(['\t'.join([str(j) for j in i]) for i in matr]))
+        print(int(my_data[0]), int(my_data[1]), int(my_data[2]))
+
+    @staticmethod
+    def valid(day, month, year):
+        if day > 31:
+            print("Введен не правильный день!")
+        elif month > 12:
+            print("Введен не правильный месяц!")
+        elif year > 2021:
+            print("Введен не правильный год!")
 
     def __str__(self):
-        return str('\n'.join(['\t'.join([str(j) for j in i]) for i in matr]))
+        return f'Дата - {Data.extract(self.day, self.month, self.year)}'
 
 
-my_first_matrix = Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[9, 8, 7], [6, 5, 4], [3, 2, 1]])
-
-print(my_first_matrix.__add__())
+Data.extract(2, 6, 1997)
+Data.valid(2, 6, 1997)
+Data.valid(55, 6, 1997)
+Data.valid(2, 18, 1997)
+Data.valid(2, 6, 2056)
